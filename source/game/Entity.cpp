4c0c23d26fb14c12fee9f3afc65cb08db24f2c98
Kelly Brown <TheBuzzSaw@Gmail.com>
2011-01-29 20:26:24 -0700

    Entity object under construction

---
+++ source/game/Entity.cpp
@@ -0,0 +1,43 @@
+#include "Entity.h"
+
+Entity::Entity() : mMass(0.0f), mRotation(0.0f), mRadius(0.0f), mHP(0),
+    mMaxHP(0)
+{
+    mPosition[0] = 0.0f;
+    mPosition[1] = 0.0f;
+
+    mVelocity[0] = 0.0f;
+    mVelocity[1] = 0.0f;
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::onPulse()
+{
+    mPosition[0] += mVelocity[0];
+    mPosition[1] += mVelocity[1];
+}
+
+void Entity::onDeath()
+{
+}
+
+void Entity::onCollision(const Entity& inEntity)
+{
+}
+
+void Entity::applyForce(vec2f inAcceleration, float inMass)
+{
+    mVelocity[0] = (inAcceleration[0] * inMass + mVelocity[0] * mMass)
+        / mMass;
+    mVelocity[1] = (inAcceleration[1] * inMass + mVelocity[1] * mMass)
+        / mMass;
+}
+
+void Entity::applyAcceleration(vec2f inAcceleration)
+{
+    mVelocity[0] += inAcceleration[0];
+    mVelocity[1] += inAcceleration[1];
+}
---
+++ source/game/Entity.h
@@ -0,0 +1,46 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <CGE/Vectors.h>
+
+class Entity
+{
+    public:
+        Entity();
+        virtual ~Entity();
+
+        virtual void render() = 0;
+        virtual void onPulse();
+        virtual void onDeath(); // when HP hit zero
+        virtual void onCollision(const Entity& inEntity);
+
+        void applyForce(vec2f inAcceleration, float inMass);
+        void applyAcceleration(vec2f inAcceleration);
+
+    protected:
+        vec2f mPosition;
+
+        /// momentum
+        vec2f mVelocity;
+        float mMass;
+
+        float mRotation; // Z-rotation
+        float mRadius; // "size" of object
+
+        unsigned int mHP;
+        unsigned int mMaxHP;
+
+    private:
+        Entity(const Entity& inEntity)
+        {
+            // no copying
+        }
+
+        Entity& operator=(const Entity& inEntity)
+        {
+            // no copying
+            return *this;
+        }
+};
+
+#endif
